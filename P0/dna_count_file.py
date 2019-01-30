7f33bffbd734f4c86ff9e271d268e1def37f7fff
amapy <andremapy@gmail.com>
2019-01-30 17:50:29 +0100

    Just testing my first project in github

--- P0/dna_count_file.py
+++ P0/dna_count_file.py
@@ -1,11 +1,20 @@
 with open("file_dna_test.txt", "r") as file:
 
+    dna_seq = []
+
     for line in file:
         line = line.lower()
         line = line.replace('\n', '')
+        dna_seq.append(line)
 
-        print("- Total length: ", len(line), "\n- A: ", line.count("a"), "\n- C: ", line.count("c"), "\n- G: ", line.count("g"), "\n- T: ", line.count("t"))
+    total_seq = []
+
+    for chain in dna_seq:
+        total_seq += chain
+
+    print("- Total length: ", len(total_seq), "\n- A: ", total_seq.count("a"), "\n- C: ", total_seq.count("c"), "\n- G: ", total_seq.count("g"), "\n- T: ", total_seq.count("t"))
 
     file.close()
+
 
 
