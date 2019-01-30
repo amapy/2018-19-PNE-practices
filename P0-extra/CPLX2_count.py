3840df616d5e26f63f5e587505ee8e4ecfb2911f
amapy <andremapy@gmail.com>
2019-01-30 17:28:37 +0100

    Just testing my first project in github

--- P0-extra/CPLX2_count.py
+++ P0-extra/CPLX2_count.py
@@ -7,11 +7,12 @@ info.close()
 info = open("cplx2.txt", "w")
 
 for line in lines:
+    line = line.replace("\n", "")
     if line[0] !=">":
         info.write(line)
         line = line.lower()
 
-    print("- A: ", line.count("a"), "\n- C: ", line.count("c"), "\n- G: ", line.count("g"), "\n- T: ", line.count("t"))
+print("- A: ", line.count("a"), "\n- C: ", line.count("c"), "\n- G: ", line.count("g"), "\n- T: ", line.count("t"))
 
 info.close()
 
