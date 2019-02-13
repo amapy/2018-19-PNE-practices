f908cec629ad19328d3da03db53903a8e29000c7
amapy <andremapy@gmail.com>
2019-02-13 18:16:58 +0100

    First program for practice 1

--- P1/Seq.py
+++ P1/Seq.py
@@ -7,11 +7,21 @@ class Seq:
         return len(self.strbase)
 
     def complement(self):
-        my_complement = self.complement()
-        return "Original sequence: {}".format(self), "Complement sequence: {}".format(my_complement)
+        my_dict = {"A": "T", "T": "A", "C": "G", "G": "C"}
+        my_sequence = ""
+        for base in self.strbase:
+            if base == "A":
+                my_sequence += my_dict.get("A")
+            elif base == "T":
+                my_sequence += my_dict.get("T")
+            elif base == "C":
+                my_sequence += my_dict.get("C")
+            elif base == "G":
+                my_sequence += my_dict.get("G")
+        return my_sequence
 
     def reverse(self):
-        my_reverse = self.reverse()
+        my_reverse = self.strbase[::-1]
         return my_reverse
 
     def count_base(self):
@@ -28,14 +38,13 @@ class Seq:
                 g_base += 1
             elif base == "T":
                 tbase += 1
-        return ("- Number of A's: {}".format(abase),
-                "\n- Number of C's: {}".format(cbase),
-                "\n- Number of G's: {}".format(g_base),
-                "\n- Number of T's: {}".format(tbase))
+        my_out = "A's: {}".format(abase),"C's: {}".format(cbase),"G's: {}".format(g_base),"T's: {}".format(tbase)
+        return my_out
 
     def perc(self):
         length = len(self.strbase)
-        return ("- The percentage of A's is: {}%".format(round(100.0 * self.strbase.count("A") / length, 1)),
+        my_out2 = ("- The percentage of A's is: {}%".format(round(100.0 * self.strbase.count("A") / length, 1)),
                 "- The percentage of C's is: {}%".format(round(100.0 * self.strbase.count("C") / length, 1)),
                 "- The percentage of G's is; {}%".format(round(100.0 * self.strbase.count("G") / length, 1)),
                 "- The percentage of T's is; {}%".format(round(100.0 * self.strbase.count("T") / length, 1)))
+        return my_out2
