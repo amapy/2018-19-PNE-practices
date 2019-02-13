a186e00fd648f12a5b558557798fef1e30f56e0d
amapy <andremapy@gmail.com>
2019-02-13 18:17:40 +0100

    Main program for pracitce 1

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -3,16 +3,17 @@ from Seq import Seq
 seq_1 = Seq("ACAGTATAGCTTAGC")
 seq_2 = Seq("GTCCCTTTAACGATTTAG")
 seq_3 = seq_1.complement()
+seq_3 = Seq(seq_3)
 seq_4 = seq_3.reverse()
+seq_4 = Seq(seq_4)
 
 my_seq = [seq_1, seq_2, seq_3, seq_4]
 
 number = 0
 
 for sequence in my_seq:
-    print(sequence)
-    len(sequence)
-    count_base(sequence)
-    perc(sequence)
-
     number += 1
+    print("SEQUENCE: ", number)
+    print("Length: ", sequence.len())
+    print(sequence.count_base())
+    print(sequence.perc())
