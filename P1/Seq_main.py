49636a464a46980bc302834632164d1b1e40d092
amapy <andremapy@gmail.com>
2019-02-13 18:27:52 +0100

    Main program for pracitce 1

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -1,5 +1,7 @@
+# First, we import the object we created in the first program
 from Seq import Seq
 
+# Now, we create the sequences and make sure they are the object we created
 seq_1 = Seq("ACAGTATAGCTTAGC")
 seq_2 = Seq("GTCCCTTTAACGATTTAG")
 seq_3 = seq_1.complement()
@@ -7,10 +9,12 @@ seq_3 = Seq(seq_3)
 seq_4 = seq_3.reverse()
 seq_4 = Seq(seq_4)
 
+# This is a list for organising the sequences
 my_seq = [seq_1, seq_2, seq_3, seq_4]
 
 number = 0
 
+# Print the statistics requested
 for sequence in my_seq:
     number += 1
     print("SEQUENCE: ", number)
