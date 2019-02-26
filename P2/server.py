505e7f2ed84b1289a1647949968bc09a2daf6fe2
amapy <andremapy@gmail.com>
2019-02-26 16:25:07 +0100

    server from practice 2

--- P2/server.py
+++ P2/server.py
@@ -1,8 +1,8 @@
 import socket
 
 # Configure the Server's IP and PORT
-PORT = 8088
-IP = "212.128.253.118"
+PORT = 8089
+IP = "127.0.0.1"
 MAX_OPEN_REQUESTS = 5
 
 # Counting the number of connections
@@ -27,13 +27,14 @@ try:
         # Print the conection number
         print("CONNECTION: {}. From the IP: {}".format(number_con, address))
 
-        # Read the message from the client, if any
+        # Read the message from the client
         msg = clientsocket.recv(2048).decode("utf-8")
         print("Message from client: {}".format(msg))
 
         # Send the messag
         message = "Hello from the amapy's server"
         send_bytes = str.encode(message)
+
         # We must write bytes, not a string
         clientsocket.send(send_bytes)
         clientsocket.close()
