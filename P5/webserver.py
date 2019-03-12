9a7f3718b5a5797ce9d051c4753972e24a90c964
amapy <andremapy@gmail.com>
2019-03-12 10:09:37 +0100

    Web server for practice 5

---
+++ P5/webserver.py
@@ -0,0 +1,70 @@
+import http.server
+import termcolor
+import socketserver
+
+
+# Define the Server's port
+PORT = 8002
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        if self.path == "/":
+            name_file = "index.html"
+        elif self.path == "/blue":
+            name_file = "blue.html"
+        elif self.path == "/pink":
+            name_file = "pink.html"
+        else:
+            name_file = "error.html"
+
+        with open(name_file, "r") as file:
+            contents = file.read()
+
+        # Generating the response message
+        self.send_response(200)  # -- Status line: OK!
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
